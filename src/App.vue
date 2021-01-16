db4d1decd3273c81b001e801b76fcc8a3f91610a
Simon Lambin <slambin@artfx.fr>
2021-01-16 15:44:39 +0100

    feat(NavBar component): Create the NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
+<script>
+import NavBar from "./components/NavBar.vue"
+
+export default {
+    name: "App",
+    components: {
+        NavBar
+    }
+}
+</script>
+
 <template>
   <div id="app">
     <div id="nav">
+      <NavBar label="Simon Lambin" button="Click !" />
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name : "NavBar",
+    props : ['label', 'button'],
+    data : function() {
+        return {
+            buttons : [
+                { index : 1, name : "About", link : "https://github.com/" },
+                { index : 2, name : "Portfolio", link : "https://www.youtube.com/" },
+                { index : 3, name : "Contact", link : "https://openclassrooms.com/fr/" }
+            ]
+        }    
+    }
+}
+</script>
+
+
+<template>
+    <div id="app">
+        <h2>Hi, i'm {{ label }}</h2>
+        <nav>
+            <ul>
+                <li v-for="button in buttons" v-bind:key="button.key">
+                    <a v-bind:href="button.link">{{ button.name }}</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+
+<style>
+h2 {
+    color: blue;
+}
+
+button {
+    text-decoration: none;
+}
+</style>
