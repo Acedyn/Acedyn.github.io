1e1ee0dff66b115e23921ff4eb7ddd9711ea734f
Simon Lambin <slambin@artfx.fr>
2021-02-09 13:14:04 +0100

    feat(VideoPlayer component): Remove the functions and use vue observable system instead

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,41 +1,16 @@
 <script>
-let videoList = [
-    "../assets/video/reel-drone.mp4",
-    "../assets/video/reel-buildingGenerator.mp4",
-    "../assets/video/reel-mazeGenerator.mp4",
-    "../assets/video/reel-environment.mp4",
-    "../assets/video/reel-mystique.mp4",
-]
-
-function playVideos() {
-    videoList.sort(() => {return 0.5 - Math.random()})
-    let video = document.getElementById("video")
-    let source = videoList[0]
-    console.log(source)
-    // video.setAttribute("src", source)
-    video.load()
-    video.play()
-}
-
-function resizeVideo() {
-    let video = document.getElementById("video")
-    if(video.offsetHeight > 300)
-    {
-        let height = (video.offsetHeight - 300) / 2 + "px"
-        video.style.bottom = height
-    }
-}
-
-
 export default {
     name: "VideoPlayer",
-    mounted() {
-        playVideos()
-        resizeVideo()
-        window.addEventListener("resize", resizeVideo)
-    },
-    destroyed() {
-        window.removeEventListener("resize", resizeVideo)
+    data: () => {
+        return {
+            videos: [
+                "../assets/video/reel-drone.mp4",
+                "../assets/video/reel-buildingGenerator.mp4",
+                "../assets/video/reel-mazeGenerator.mp4",
+                "../assets/video/reel-environment.mp4",
+                "../assets/video/reel-mystique.mp4",
+            ]
+        }
     }
 }
 </script>
@@ -44,7 +19,7 @@ export default {
 <template>
     <div id="videoplayer">
         <video id="video">
-            <source src="../assets/video/reel-buildingGenerator.mp4" type="video/mp4">
+            <source controls v-bind:src="videos[0]" type="video/mp4" autoplay="true">
             Video could not be loaded
         </video>
     </div>
@@ -54,7 +29,7 @@ export default {
 <style scoped>
 #videoplayer {
     overflow: hidden;
-    max-height: 300px;
+    max-height: 500px;
     display: grid;
     margin-top: 20px;
 }
