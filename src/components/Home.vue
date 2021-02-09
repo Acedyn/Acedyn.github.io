c578af0b70c6cc0fdfdd7bea0c250f01a2bb19ec
Simon Lambin <slambin@artfx.fr>
2021-02-09 15:47:08 +0100

    Delete vue config file

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,7 +16,7 @@ export default {
     <div id="home">
         <PageTitle title="HOME"/>
         <VideoPlayer/>
-        <span/>
+        <p>My name is Simon, I am currently a student in computer programming at ArtFX. I started as a 3D generalist and now focus more on programming. My main softwares are Houdini and Unreal, I am highly interested in procedual modeling but also in procedural in general (Animation, FX...)</p>
     </div>
 </template>
 
@@ -30,5 +30,9 @@ export default {
 
 VideoPlayer {
     display: grid;
+}
+
+p {
+    text-align: center;
 }
 </style>
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: 'Acedyn.github.io'
-}
